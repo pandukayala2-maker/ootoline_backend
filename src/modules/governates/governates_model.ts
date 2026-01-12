2dea6c2e47ba0c83dfbe5cd1f4ebccc1d20c11cf
pandukayala2-maker <pandukayala2@gmail.com>
2026-01-12 12:07:25 +0300

    governates

--- src/modules/governates/governates_model.ts
+++ src/modules/governates/governates_model.ts
@@ -5,7 +5,6 @@ interface GovernateDocument extends BaseDocument {
     name: string;
     value: number;
     vendor_id?: Types.ObjectId | string | null;
-    admin_governate_id?: Types.ObjectId | string | null;
     is_disabled: boolean;
 }
 const governateSchema = new Schema({
@@ -13,13 +12,11 @@ const governateSchema = new Schema({
     vendor_id: { type: String },
     value: { type: Number },
     is_disabled: { type: Boolean, default: false },
-    admin_governate_id: { type: String } // Reference to admin governate if this is a vendor copy
 });
 
 governateSchema.add(baseSchema);
 
-// Create compound unique index on name and vendor_id
-// This allows same name for different vendors (null vendor_id = admin)
+
 governateSchema.index({ name: 1, vendor_id: 1 }, { unique: true });
 
 const GovernateModel = model<GovernateDocument>('governates', governateSchema);
